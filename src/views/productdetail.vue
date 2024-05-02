eeb1396b7248ae3124bc0927b6f8a1046caebf89
tekarr <137101567+tekarr@users.noreply.github.com>
2024-05-02 09:23:30 +0200

    Update productdetail.vue

--- src/views/productdetail.vue
+++ src/views/productdetail.vue
@@ -2,27 +2,27 @@
 
     <navbarr/>
 
-    <div class="p-32 mt-14 flex justify-between">
+    <div class="p-32 mt-14 md:p-10 lg:p-32  md:mt-28 flex flex-col md:flex-row justify-between">
         
         <div class="grid gap-2" v-for="card in cards" :key="card.id">
             <div>
-                <img class="h-auto w-3/4 ml-0 rounded-3xl " :src="selectedImage"  alt="">
+                <img class="h-auto w-3/4 md:w-3/4 lg:w-4/5 xl:w-3/4 ml-0 rounded-3xl " :src="selectedImage"  alt="">
             </div>
-            <div class="grid grid-cols-5 w-3/4 gap-3 rounded-3xl">
+            <div class="grid grid-cols-5 w-3/4 gap-3 md:w-3/4 lg:w-4/5 xl:w-3/4  rounded-3xl">
                 <div v-for="pr in prs" :key="pr.id">
-                    <img class="h-auto rounded-3xl border-4 hover:border-pink-600 hover:cursor-pointer transition-all" :src="pr.image" alt="" @click="switchimg(pr.image)" >
+                    <img class="h-auto rounded-3xl border-4 hover:border-pink-600 hover:cursor-pointer transition-all " :src="pr.image" alt="" @click="switchimg(pr.image)" >
                 </div>
             </div>
         </div>
 
     
-        <div class=" w-1/2 " v-for="card in cards" :key="card.id">
+        <div class="  w-full md:w-full lg:w-1/3 xl:w-1/2 mt-5 md:mt-0" v-for="card in cards" :key="card.id">
             <div class="p-10 md:pr-10">
                 <p class="text-lg md:text-3xl font-extrabold pb-1 text-start">{{ id }}</p>
                 <p class="text-lg md:text-base text-start  font-bold opacity-30 mb-1 md:pb-0">{{ card.store }}</p>
                 <div class="text-lg md:text-2xl font-bold p-3 text-start">{{ card.price }}$</div>
                 <p class="pb-1 text-base md:text-start w-80">{{ card.description }}</p>
-                <div class="flex flex-col md:flex-row justify-between items-center p-10">
+                <div class="flex flex-col md:flex-row justify-around items-center p-1 pt-10">
                     <router-link to="/" class="text-pink-600 hover:text-white border border-pink-600 bg-white hover:bg-pink-600 focus:ring-4 focus:outline-none focus:ring-pink-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 "> Add to Cart</router-link>
                     <router-link to="/" class="text-pink-600 hover:text-white border border-pink-600 bg-white hover:bg-pink-600 focus:ring-4 focus:outline-none focus:ring-pink-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 "> Buy </router-link>
                 </div>
