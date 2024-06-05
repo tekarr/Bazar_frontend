c42ba50c8bdb75fef0f3d3ec67c68cba71d45564
tekarr <137101567+tekarr@users.noreply.github.com>
2024-06-05 16:36:05 +0200

    Update profile.vue

--- src/views/customer/profile.vue
+++ src/views/customer/profile.vue
@@ -2,25 +2,28 @@
     
     <navbarr/>
 
-    <div class="flex items-start justify-start mt-28 bg-gray-100 py-24 ">
-    <form @submit.prevent="submitForm" class="max-w-sm mx-10 mt-4 ">
-        
+    
+    <div class="flex items-start justify-start mt-28 bg-gray-100 py-20 ">
+        <form @submit.prevent="submitForm" class="max-w-sm mx-10 mt-4 ">
+            
+        <p class="text-3xl font-medium mb-10 ml-3">Profile</p>
+
         <div class="mb-5 text-start">
             <label class="font-normal pl-5" for="username">Name</label>
             <input type="text" id="username" v-model="username" placeholder="username" 
-            class="w-full mt-4 bg-white px-20 py-2 rounded-3xl focus:outline-none focus:ring focus:ring-pink-500 " required>
+            class="w-full mt-4 bg-white mr-20 pl-4 py-2 rounded-3xl focus:outline-none focus:ring focus:ring-pink-500 " required>
         </div>
 
         <div class="mb-5 text-start">
             <label class="font-normal pl-5" for="email">Email</label>
             <input type="email" id="email" v-model="email" placeholder="name@gmail.com" 
-            class="w-full mt-4 bg-white px-20 py-2 rounded-3xl focus:outline-none focus:ring focus:ring-pink-500 " required>
+            class="w-full mt-4 bg-white mr-20 pl-4 py-2 rounded-3xl focus:outline-none focus:ring focus:ring-pink-500 " required>
         </div>
         
         <div class="mb-5 text-start">
             <label class="font-normal pl-5" for="email">Password</label>
             <input type="password" id="password" v-model="password" placeholder="password" 
-            class="w-full mt-4 bg-white px-20 py-2 rounded-3xl focus:outline-none focus:ring focus:ring-pink-500 " required>
+            class="w-full mt-4 bg-white mr-20 pl-4 py-2 rounded-3xl focus:outline-none focus:ring focus:ring-pink-500 " required>
         </div>
         <button type="submit" class=" m-5 text-white bg-pink-600 hover:bg-pink-700 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-3xl text-sm px-5 py-2.5 text-center ">Save Profile</button>
     </form>
