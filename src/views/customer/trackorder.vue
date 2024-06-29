4cdb9a3ac0d9f60c2db30fdcee8a6cfa905d5c1f
tekarr <137101567+tekarr@users.noreply.github.com>
2024-06-29 16:59:08 +0200

    .

--- src/views/customer/trackorder.vue
+++ src/views/customer/trackorder.vue
@@ -9,33 +9,34 @@
     </div>
     
 
-<div class="flex justify-center">
-    <div class="  grid grid-cols-1 w-3/4 gap-2  rounded-3xl py-4">
-        
-
-        <div class="text-start pt-4 mx-32 bg-white ">
+    <div class="flex justify-center">
+        <div class="  grid grid-cols-1 w-3/4 gap-2  rounded-3xl py-4">
             
-            <div class="bg-gray-200 rounded-3xl h-2 ">
-                <div :class="getWidthClass(order.order_status)"  class="bg-pink-600 rounded-3xl text-pink-600 text-xs  h-2 flex justify-start items-center 
-                transform translate-x-100 translate-y-0 ">
+
+            <div class="text-start pt-4 mx-32 bg-white ">
+                
+                <div class="bg-gray-200 rounded-3xl h-2 ">
+                    <div :class="getWidthClass(order.order_status)"  class="bg-pink-600 rounded-3xl text-pink-600 text-xs  h-2 flex justify-start items-center 
+                    transform translate-x-100 translate-y-0 ">
+                    </div>
                 </div>
-            </div>
 
 
-            <div class="py-6 flex justify-between">
-                <p>Pending</p>
-                <p>Processing</p>
-                <p>Shipping</p>
-                <p>Delivered</p>
+                <div class="py-6 flex justify-between">
+                    <p>pending</p>
+                    <p>processing</p>
+                    <p>ready_for_shipment</p>
+                    <p>shipped</p>
+                    <p>delivered</p>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
     <div class="flex justify-center p-10 ">
-        <p class="text-xl font-bold pb-5 ">Order Status  <span class="p-2">|</span>  <span class=" text-pink-600">{{ order.order_status }}</span></p>
-        <p class="text-xl font-bold pl-10">Subtotal : {{ order.order_total }}</p>
+        <p class="text-xl font-bold pb-5 "> Order Status  <span class="p-2">|</span>  <span class=" text-pink-600">{{ order.order_status }}</span></p>
+        <p class="text-xl font-bold pl-10"> Subtotal : {{ order.order_total }}</p>
     </div>
 
     <div class="flex justify-center cursor-pointer hover:text-pink-600">
@@ -58,7 +59,8 @@ import axiosClient from '@/axios';
             try {
             const response = await axiosClient.get(`api/customer/orders/${id}`);
             this.order = response.data.data;
-            console.log(this.order.order_total);
+            console.log(this.order);
+            //this.order.order_status = 'delivered'
             } catch (error) {
             console.error(`There was an error fetching the order: ${error}`);
             }
@@ -68,11 +70,13 @@ import axiosClient from '@/axios';
         switch (status) {
             case 'pending':
             return 'w-[4%]';
-            case 'Processing':
-            return 'w-[35%]';
-            case 'Shipping':
-            return 'w-[65%]';
-            case 'Delivered':
+            case 'processing':
+            return 'w-[25%]';
+            case 'ready_for_shipment':
+            return 'w-[50%]';
+            case 'shipped':
+            return 'w-[75%]';
+            case 'delivered':
             return 'w-[100%]';
             default:
             return 'w-[4%]';
