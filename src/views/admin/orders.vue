000a476bcf03b795ea9c8a3f75f15e12e5f34385
tekarr <137101567+tekarr@users.noreply.github.com>
2024-08-01 17:43:21 +0200

    .

--- src/views/admin/orders.vue
+++ src/views/admin/orders.vue
@@ -26,7 +26,7 @@
                 </thead>
                 <tbody>
                     <tr v-for="order in orders" :key="order.id" class="bg-white text-center " >
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                             {{order.id}}
                         </th>
                         <td class="">
