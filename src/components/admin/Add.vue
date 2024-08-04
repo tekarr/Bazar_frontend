a033c624e56d3d1844007dbfd5da311b3f115470
tekarr <137101567+tekarr@users.noreply.github.com>
2024-08-04 17:33:17 +0200

    .

--- src/components/admin/Add.vue
+++ src/components/admin/Add.vue
@@ -17,23 +17,43 @@
             class="w-full mt-4 bg-slate-50 mr-10 pl-4 py-2 rounded-3xl focus:outline-none focus:ring focus:ring-pink-500 " required>
         </div>
         
-        <div class="mb-5 text-start">
+        <!-- <div class="mb-5 text-start">
             <label class="font-bold pl-5" for="email">Password</label>
             <input type="password" id="password" v-model="user.password" placeholder="password" 
             class="w-full mt-4 bg-slate-50 mr-10 pl-4 py-2 rounded-3xl focus:outline-none focus:ring focus:ring-pink-500 " >
-        </div>
+        </div> -->
 
         <!-- Role -->
         <div class="">
-        <p class="">Role </p>    
+        <p class="pl-5">Role </p>    
             <select v-model="user.role_id"  class="p-2 my-4 px-4  border-2 border-pink-600 rounded-xl" required>
                 <option disabled value ="">Please select one</option>
                 <option value=3>customer</option>
                 <option value=2>vendor</option>
                 <option value=1 >admin</option>
             </select>
-            <p>{{user.role_id}}</p>
         </div>
+
+        <hr class="my-4">
+
+        <div v-if="user.role_id == 2">
+            <!-- storename -->
+            <div class="mb-5 text-start">
+                <label class="font-bold pl-5" for="storename">Storename</label>
+                <input type="text" id="storename" v-model="user.store_name" placeholder="username" 
+                class="w-full mt-4 bg-slate-50 mr-10 pl-4 py-2 rounded-3xl focus:outline-none focus:ring focus:ring-pink-500 " required>
+            </div>
+
+            <!-- category -->
+            <div class="">
+                <p class="pl-5">Category </p>    
+                <select v-model="user.category"  class=" p-2 my-4 px-4  border-2 border-pink-600 rounded-xl" required>
+                    <option disabled value ="">Please select one</option>
+                    <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.name }}</option>
+                </select>
+            </div>
+        </div>
+
 
         <button type="submit" class=" m-5 text-white bg-pink-600 hover:bg-pink-700 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-3xl text-sm px-5 py-2.5 text-center ">Add</button>
     </form>
@@ -49,19 +69,43 @@ export default {
         return {
         user:{
             role_id: '',
+            store_name: '',
+            category:'',
         },
+        categories: [],
         };
     },
     methods: {
-        async submitForm() {
-        try {
+        async fetchCategories() {
+            try {
+                const response = await axiosClient.get('api/admin/categories');
+                this.categories = response.data;
+                console.log(response);
+                console.log(this.categories);
+            } catch (error) {
+                console.error(error);
+            }
+        },
+    async submitForm() {
+    if (this.user.role_id != 2){
+        delete this.user.store_name;
+        delete this.user.category;
+    }
+    this.user.category = this.user.category.toString();
+    try {
         const response = await axiosClient.post('api/admin/users',this.user);
         this.user = response.data;
         console.log(this.user);
-    } catch (error) {
+    }
+    catch (error) {
         console.error(error);
+        console.log(this.user);
+        console.log(this.userData);
     }
     }
+    },
+    mounted() {
+    this.fetchCategories();
     }
 };
 </script>
