f056c75f8caefa0cb14165197e666649b29cc494
tekarr <137101567+tekarr@users.noreply.github.com>
2024-08-13 13:11:45 +0200

    .

--- src/components/admin/Sidbar.vue
+++ src/components/admin/Sidbar.vue
@@ -32,7 +32,7 @@
                     <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
                 </svg>
                 <span class="flex-1 ms-3 whitespace-nowrap ">Notifications</span>
-                <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-gray-100 bg-pink-300 rounded-full">3</span>
+                <!-- <span class="inline-flex items-center justify-center w-4 h-4 p-4 ms-3 text-xs font-medium text-gray-100 bg-pink-400 rounded-full">{{ unread }}</span> -->
             </router-link>
             </li>
             <li>
@@ -90,13 +90,25 @@
 </template>
 
 <script>
+import axiosClient from '@/axios';
+
     export default {
     data() {
     return {
     isActive: false,
     hover: true,
+    // unread:''
     };
     },
+    // async created() {
+    // try {
+    //     const response = await axiosClient.get('api/admin/notifications');
+    //     this.unread = response.data.data.filter(message => message.read_at).length
+    //     //this.unread = response.data.data.filter(message => message.read_at === 'Unread').length
+    //     } catch (error) {
+    //     console.error(error);
+    //     }
+    // },
     methods: {
         sidebarLinkClass(routePath) {
         return {
--- src/views/vendor/vnotification.vue
+++ src/views/vendor/vnotification.vue
@@ -1,56 +1,70 @@
 <template>
-
-        <div class="relative overflow-x-auto">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 rounded-3xl overflow-hidden">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
-                    <tr>
-                        <th scope="col" class="px-6 py-3">
-                            username
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            massage
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            time
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="user in users" :key="user.id" class="bg-white  " >
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                            {{user.name}}
-                        </th>
-                        <td class="px-6 py-4">
-                            {{user.massage}}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{user.time}}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-
+    <div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 rounded-3xl overflow-hidden">
+        <thead class="text-xs  text-gray-700 uppercase bg-gray-50 ">
+        <tr>
+            <th scope="col" class="px-6 py-3 ">
+            Message
+            </th>
+            <th scope="col" class="px-6 py-3 ">
+            Time
+            </th>
+            <!-- <th scope="col" class="px-6 py-3">
+            Status
+            </th> -->
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="notification in notifications" :key="notification.id" class="bg-white  cursor-pointer hover:text-pink-500" @click="handleClick(notification)">
+            <td class="px-6 py-4">
+            {{ notification.data.body }}
+            </td>
+            <td class="px-1 py-4 text-pink-500">
+            {{ notification.created_at }}
+            </td>
+            <!-- <td class="px-6 py-4 text-pink-600">
+            {{ notification.read_at }}
+            </td> -->
+        </tr>
+        </tbody>
+    </table>
+    </div>
 </template>
 
 <script>
-import Sidbar from '@/components/vendor/Sidbar.vue'
-import Navbar from '@/components/vendor/navbar.vue'
-    export default {
-    components: { Sidbar, Navbar },
-    data(){
-        return{
-            users:[
-                {
-                    name:'Ahmed',
-                    massage:'new user rigester',
-                    time:'1 houre ago'
-                }
-            ],
-        }
+import axiosClient from '@/axios';
+
+export default {
+data(){
+    return{
+        notifications: [],
+        unread:''
     }
-        
+},
+async created() {
+try {
+    const response = await axiosClient.get('api/vendor/notifications');
+
+    this.notifications  = response.data;
+    this.unread = response.data.data.filter(message => message.read_at === 'Unread').length
+    } catch (error) {
+    console.error(error);
     }
+}, 
+methods: {
+handleClick(notification) {
+     // Extract the resource part from the URL
+    const urlParts = notification.data.url.split('/');
+    const resource = urlParts[1];
+
+    // Construct the new URL with the vendor prefix
+    const url = `/vendor/${resource}`;
+    this.$router.push(url);
+    }
+}
+
+    
+}
 </script>
 
 <style lang="scss" scoped>
