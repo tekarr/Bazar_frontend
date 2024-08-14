3e652e389673690b97920c8f394ff24d1a8d1107
tekarr <137101567+tekarr@users.noreply.github.com>
2024-08-14 17:12:45 +0200

    Update cards.vue

--- src/components/customer/cards.vue
+++ src/components/customer/cards.vue
@@ -20,7 +20,7 @@
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                     </svg>
                 </div>
-                <input type="search" v-model="searchvalue" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border-2 border-gray-300 rounded-3xl bg-gray-50 focus:outline-none focus:ring focus:ring-pink-500 " placeholder="Search Stores, Products..." required @input="search"/>
+                <input type="search" v-model="searchvalue" id="default-search" class="block w-full p-3 ps-10 text-sm text-gray-900 border-2 border-gray-300 rounded-3xl bg-gray-50 focus:outline-none focus:ring focus:ring-pink-500 " placeholder="Search Stores, Products..." required @input="search"/>
             </div>
         </div>    
 
